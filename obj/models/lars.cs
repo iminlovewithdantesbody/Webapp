aaadefcef1582773bcd349ba1ba608655cd2c5c4
iminlovewithdantesbody <oliver.granasjungdalen@gmail.com>
2026-02-17 10:30:40 +0100

    [add] modelpages

---
+++ obj/models/lars.cs
@@ -0,0 +1,11 @@
+namespace  webapp.models 
+
+public class ClientCreateFormModel 
+{
+
+    public string ClientName {get; set; } = null!;
+
+    public string ContactPerson {get; set; } = null!
+    
+    
+}
